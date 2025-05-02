16dc84b6a33ecea5797238b2702af7195172b562
mstrong90 <64449912+mstrong90@users.noreply.github.com>
2025-05-01 20:49:26 -0700

    .

--- public/flappy_quakks/speedRunSettings.js
+++ public/flappy_quakks/speedRunSettings.js
@@ -1,7 +1,7 @@
-export const FPS            = 30;
-export let PIPE_SPEED       = 200;   // px/s
-export let SPAWN_INT        = 1.0;   // seconds between pipes
-export let PIPE_GAP         = 180;   // px
-export const GRAVITY        = 950;   // px/s²
-export const FLAP_V         = -250;  // px/s
-export const HITBOX_PADDING = 4;     // px inset for collision
+export const FPS            = 60;
+export let PIPE_SPEED       = 350;   // px/s
+export let SPAWN_INT        = 0.75;   // seconds between pipes
+export let PIPE_GAP         = 175;   // px
+export const GRAVITY        = 900;   // px/s²
+export const FLAP_V         = -300;  // px/s
+export const HITBOX_PADDING = 5;     // px inset for collision
